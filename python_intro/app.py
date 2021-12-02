9ae9d74ecf8d5ef424ddc1bb5af2b65746472dde
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2021-12-02 10:08:36 +0100

    added python baisics

---
+++ python_intro/app.py
@@ -0,0 +1,33 @@
+# print('Program na scitani dvou cisel A+B')
+# a = float(input('Zadej A: '))
+# b = float(input('Zadej B: '))
+# suma = a + b
+# print(f"Soucet je {suma:.2f}.")
+
+# t = "ahoj kamo"
+# t.upper().replace()
+
+
+# 'ahoj'
+
+# +
+# -
+# *
+# /
+# //
+# %
+# **
+# # +=
+# x = 10
+# x **= 3
+# print(x)
+
+from turtle import *
+shape('turtle')
+speed(1)
+
+for i in range(4):
+    forward(50)
+    left(90)
+
+exitonclick()
