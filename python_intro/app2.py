5792bc759a275ddc8fe1d8d05a7737965b504b21
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2021-12-02 12:08:36 +0100

    added python simple examples

---
+++ python_intro/app2.py
@@ -0,0 +1,30 @@
+
+# print("Program na secteni A+B")
+# a = float(input("Zadej cislo A: "))
+# b = float(input("Zadej cislo B: "))
+
+# soucet = a + b
+# print(f"Soucet je {soucet:.2f}")
+
+
+# t = "ahoj kamo"
+# print(t[3])
+# print(t[-3])
+# print(t[3:-3])
+# print(t[:-3])
+
+# s = t.upper().replace('O', 'x')
+
+# print(t)
+# print(s)
+
+from turtle import *
+
+shape('turtle')
+speed(1)
+
+for i in range(4):
+    forward(50)
+    left(90)
+
+exitonclick()
