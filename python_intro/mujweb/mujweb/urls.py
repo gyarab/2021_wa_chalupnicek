65ddc1dcc2f8178f4a48598bd423b83e1e1eda71
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2022-01-27 12:51:22 +0100

    make templates dry using base template

--- python_intro/mujweb/mujweb/urls.py
+++ python_intro/mujweb/mujweb/urls.py
@@ -19,7 +19,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('', TemplateView.as_view(template_name='one.html'), name='one'),
     path('two', TemplateView.as_view(template_name='two.html'), name='two'),
 ]
