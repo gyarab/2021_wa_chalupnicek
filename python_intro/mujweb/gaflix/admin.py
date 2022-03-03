6151f6f208185248fe7f2df8d984825886a6a0a1
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2022-03-03 10:16:50 +0100

    improved movies admin (filter, columns, edit)

--- python_intro/mujweb/gaflix/admin.py
+++ python_intro/mujweb/gaflix/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Movie, Actor, Director, Category
 
 class ActorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'birth_date']
+    list_display = ['pk', 'name', 'birth_date', 'sex']
+    list_display_links = ['pk', 'name']
     search_fields = ['name']
+    list_filter = ['sex']
+    list_editable = ['birth_date', 'sex']
+    date_hierarchy = 'birth_date'
 
 admin.site.register(Movie)
 admin.site.register(Actor, ActorAdmin)
--- python_intro/mujweb/gaflix/models.py
+++ python_intro/mujweb/gaflix/models.py
@@ -11,10 +11,21 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
+SEX_MALE = 'male'
+SEX_FEMALE = 'female'
+SEX_OTHER = 'other'
+
+SEX_CHOICES = [
+    [SEX_MALE, 'Muž'],
+    [SEX_FEMALE, 'Žena'],
+    [SEX_OTHER, 'Jiné'],
+]
+
 class Actor(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True) 
     birth_date = models.DateField(null=True, blank=True)
+    sex = models.CharField(max_length=32, choices=SEX_CHOICES, default=SEX_MALE)
 
     def __str__(self):
         return self.name
