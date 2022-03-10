d389c768078d54375ea6bd5e26961385625a49d6
Václav Chalupníček <vaclav.chalupnicek@gyarab.cz>
2022-03-10 12:33:33 +0100

    added category filtering

--- python_intro/mujweb/gaflix/views.py
+++ python_intro/mujweb/gaflix/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Category
+
+def category_detail(request, category_id):
+    context = {
+        'movies': Movie.objects.filter(categories__id=category_id),
+        'categories': Category.objects.all()
+    }
+    return render(request, 'homepage.html', context)
 
 def movielist(request):
     context = {
-        'movies': Movie.objects.all()
+        'movies': Movie.objects.all(),
+        'categories': Category.objects.all()
     }
     return render(request, 'homepage.html', context)
